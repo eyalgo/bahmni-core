800b71c295e12cd1800fe72ff9d8f7aa33de6034
rgrnwd <rgrnwd@thoughtworks.com>
2018-05-31 09:39:17 +0200

    [Roni] trying another style of submitting the get request. updated the URL structure

--- bahmnicore-omod/src/test/java/org/bahmni/module/bahmnicore/web/v1_0/controller/search/BahmniPatientSearchControllerIT.java
+++ bahmnicore-omod/src/test/java/org/bahmni/module/bahmnicore/web/v1_0/controller/search/BahmniPatientSearchControllerIT.java
@@ -1,46 +1,35 @@
 package org.bahmni.module.bahmnicore.web.v1_0.controller.search;
 
+import org.bahmni.module.bahmnicore.web.v1_0.BaseIntegrationTest;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.openmrs.module.webservices.rest.web.RestConstants;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.Assert.assertEquals;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@WebAppConfiguration
-@ContextConfiguration(locations = {"classpath:ControllerTestingApplicationContext.xml"}, inheritLocations = true)
-public class BahmniPatientSearchControllerIT {
-
-    @Autowired
-    private WebApplicationContext webApplicationContext;
-
-    private MockMvc mockMvc;
+public class BahmniPatientSearchControllerIT extends BaseIntegrationTest {
 
     @Before
     public void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
     }
 
-    @Ignore("TOFIX")
     @Test
     public void shouldReturnStatusOK() throws Exception {
-        mockMvc.perform(get("http://www.google.com")).andExpect(status().isOk());
+        MockHttpServletRequest request = newGetRequest("/rest/v1/bahmnicore/search/patient");
+        MockHttpServletResponse response = handle(request);
 
-        String url = "openmrs/ws/rest/" + RestConstants.VERSION_1 + "/bahmnicore/search/patient";
-
-        mockMvc.perform(get(url).contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk());
+        assertEquals(200, response.getStatus());
     }
+//
+//    @Test
+//    public void shouldReturnStatusOK() throws Exception {
+//        mockMvc.perform(get("http://www.google.com")).andExpect(status().isOk());
+//
+//        String url = "openmrs/ws/rest/" + RestConstants.VERSION_1 + "/bahmnicore/search/patient";
+//
+//        mockMvc.perform(get(url).contentType(MediaType.APPLICATION_JSON))
+//                .andExpect(status().isOk());
+//    }
 
 }
